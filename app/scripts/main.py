7c701581af920b9854281bd87ee01fc9d7194aa3
JoseLuisGallardo <jgalmun939@g.educaand.es>
2024-12-01 17:23:17 +0100

    Update main.py

--- app/scripts/main.py
+++ app/scripts/main.py
@@ -4,10 +4,14 @@ def main():
     frase = "" 
     while frase.lower() != "salir":    
         frase = input("Introduce una frase (o escribe 'salir' para terminar): ")        
-        # Comprobar si es palíndromo 
-        if es_palindromo(frase): 
-            print("La frase es palíndroma.") 
-        else: 
-            print("La frase no es palíndroma.")
-    else:
-         print("Programa finalizado.") 
+        try:
+            if frase.lower() == "salir":
+                print("Programa finalizado.")
+                break
+            # Comprobar si es palíndromo 
+            if es_palindromo(frase): 
+                print("La frase es palíndroma.") 
+            else: 
+                print("La frase no es palíndroma.")
+        except Exception as e:
+            print(f"Error: {e}")
