5dd866491fc362e025c0e290efe18394411bf825
JoseLuisGallardo <jgalmun939@g.educaand.es>
2024-12-02 09:43:15 +0100

    update

--- tests/test_palindromo.py
+++ tests/test_palindromo.py
@@ -3,39 +3,39 @@ from app.scripts.charfun import es_palindromo
 
 class TestEsPalindromo(unittest.TestCase):
 
-    def test_cadena_palindroma(self):
-        cadenas_pal = ["elle", "Llell", "Anita lava la tina"]  # Lista de cadenas palíndromas
-        for cadena in cadenas_pal:
+    def test_lista(self):
+        list = ["ala", "Llell", "Anita lava la tina"]  # Lista de cadenas palíndromas
+        for aux in list:
             self.assertTrue(
-                es_palindromo(cadena),
-                f"La cadena '{cadena}' debería ser un palíndromo."
+                es_palindromo(aux),
+                f"La cadena '{aux}' debería ser un palíndromo."
             )
 
-    def test_cadena_no_palindroma(self):
-        cadenas_no_pal = ["hola", "frase", "cadena"]  # Lista de cadenas no palíndromas
-        for cadena in cadenas_no_pal:
+    def test_lista_false(self):
+        not_chain = ["hola", "frase", "cadena"]  # Lista de cadenas no palíndromas
+        for aux in not_chain:
             self.assertFalse(
-                es_palindromo(cadena),
-                f"La cadena '{cadena}' no debería ser un palíndromo."
+                es_palindromo(aux),
+                f"La cadena '{aux}' no debería ser un palíndromo."
             )
 
-    def test_cadena_vacia(self):
+    def test_list_void(self):
         self.assertTrue(
             es_palindromo(""),
             "Una cadena vacía debería ser considerada un palíndromo."
         )
 
-    def test_cadena_con_espacios_y_tildes(self):
+    def test_withspace(self):
         self.assertTrue(
             es_palindromo("Amo la paloma"),
             "La cadena con espacios y mayúsculas debería ser un palíndromo."
         )
 
-    def test_excepciones_tipo_incorrecto(self):
-        entradas_invalidas = [12, [1, 2, 3], {"clave": "valor"}, None]
-        for entrada in entradas_invalidas:
-            with self.assertRaises(TypeError, msg=f"Se esperaba TypeError con la entrada {entrada}."):
-                es_palindromo(entrada)
+    def test_list_incorectType(self):
+        listFalse = [12, [1, 2, 3], {"clave": "valor"}, None]
+        for aux in listFalse:
+            with self.assertRaises(TypeError, msg=f"Se esperaba TypeError con la entrada {aux}."):
+                es_palindromo(aux)
 
     def test_assert_equal(self):
         # Comparar resultados explícitos
